525a532aaa9613ab61fae3fcc904ea4d40c6f959
Diego Paiva <diiegopaiivam@gmail.com>
2019-06-23 20:43:07 -0300

    Second commit

---
+++ resources/views/admin/balance/index.blade.php
@@ -0,0 +1,35 @@
+@extends('adminlte::page')
+
+@section('title', 'Saldo')
+
+@section('content_header')
+    <h1>Saldo</h1>
+
+    <ol class="breadcrumb">
+        <li><a href="">Dashboard</a></li>
+        <li><a href="">Saldo</a></li>
+    </ol>
+@stop
+
+@section('content')
+
+    <div class="box">
+        <div class="box-header">
+            <a href="{{ route('balance.deposit') }}" class="btn btn-primary"> <i class="fas fa-cash-register"></i> Recarregar</a>
+            <a href="" class="btn btn-danger">Sacar</a>
+        </div>
+
+        <div class="box-body">
+            <div class="small-box bg-green">
+                <div class="inner">
+                <h3>R$ {{ number_format ($amount , 2 , "," , 00) }}</h3>
+                </div>
+                <div class="icon">
+                <i class="ion ion-cash"></i>
+                </div>
+                <a href="#" class="small-box-footer">Historico<i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        
+    </div>
+@stop
---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+Route::group(['middleware' => ['auth'], 'namespace' => 'Admin', 'prefix' => 'admin'], function(){
+
+    Route::post('deposit', 'BalanceController@depositStore')->name('deposit.store');
+    Route::get('deposit', 'BalanceController@deposit')->name('balance.deposit');
+    Route::get('balance', 'BalanceController@index')->name('admin.balance');
+    Route::get('/', 'AdminController@index')->name('admin.home');
+
+});
+
+Route::get('/', 'Site\SiteController@index')->name('home');
+
+Auth::routes();
+
+
